e0549f4ff906c91293fec6112880f118415b7dbd
Tucker <thzischka@ucdavis.edu>
2021-02-04 18:01:50 -0800

    I didn't not finish Data Parse

--- main.cpp
+++ main.cpp
@@ -17,21 +17,47 @@ int main() {
     // repeat
 
     ifstream inFile;
-    ifstream Database;
+    ifstream database;
     inFile.open("C:\\Users\\tuckr\\OneDrive\\Documents\\EEC 173\\Assignment1\\IPlist.txt");
-    Database.open("C:\\Users\\tuckr\\OneDrive\\Documents\\EEC 173\\Assignment1\\DB_091803.txt");
+    database.open("C:\\Users\\tuckr\\OneDrive\\Documents\\EEC 173\\Assignment1\\DB_091803.txt");
     if (!inFile) {
         cerr << "Unable to open file IPlist.txt";
         exit(1);   // call system to stop
     }
-    if (!Database) {
+    if (!database) {
         cerr << "Unable to open file Database";
         exit(1);   // call system to stop
     }
 
 
+    int mask[200000];
+    int AS[200000];
+
+
+    int index = 0;          //used to iterate through database parse
+    string databaseIP;  //database ip temp variable
+    int ip [4];         //ip temp variables
+    while (!database.eof()) {                                              //iterate until end of file
+        database >> databaseIP;                                         // input next ip address
+        int j = 0;                                                      // Initialize the J
+        int oldj = 0;                                                   // Initialize the oldJ
+        for (int i = 0; i < 4; i++) {                                   //For loop to get ip address
+            while (databaseIP[j] != '.' && j != databaseIP.size()) {        //Find end of byte string
+                j++;                                                    //Iterate
+            }
+            ip[i] = stoi(databaseIP.substr(oldj,j - oldj + 1));             //insert
+            oldj = j+1;         // iterate
+            j++;                // iterate
+        }
+        database >> mask[index];
+        database >> AS[index];
+        cout << ip[0] << "." << ip[1] << "." << ip[2] << "." << ip[3] << ' ' << mask[index] << ' ' << AS[index] << endl;
+        // Instert Binary Conversion code here
+
+        index++;
+    }
+
     string searchIP;
-    int ip [4];
     while (!inFile.eof()) {                                              //iterate until end of file
         inFile >> searchIP;
         int j = 0;                                                      // Initialize the J
@@ -44,20 +70,20 @@ int main() {
             oldj = j+1;         // iterate
             j++;                // iterate
         }
-        cout << ip[0] << "." << ip[1] << "." << ip[2] << "." << ip[3] << endl;
+        //Display each entry to terminal : cout << ip[0] << "." << ip[1] << "." << ip[2] << "." << ip[3] << endl;
     }
-            cout << "done" << endl;
-    /*
-    if (temp1 > temp2) {
-        cout << "It works" << endl;
-    }
-    if (temp1 < temp2){
-        cout << "It also works" << endl;
-    }
-    */
-
-
 
     inFile.close();
+    database.close();
     return 0;
 }
+
+
+/*
+  if (temp1 > temp2) {
+      cout << "It works" << endl;
+  }
+  if (temp1 < temp2){
+      cout << "It also works" << endl;
+  }
+  */
